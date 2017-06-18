0ebe25a1edc3bb71d54cb72f877e394457e92306
Matheus Teles <teles.matheus@hotmail.com>
2017-06-18 20:39:23 -0300

    configuracao do banco de dados

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -17,10 +17,12 @@ public class Configuracao {
 	@Bean
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Drive");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
 		dataSource.setUsername("root");
 		dataSource.setPassword("123456");
 		return dataSource;
+		
+
 	}
 }
--- src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -1,10 +1,20 @@
 package br.com.alura.listavip;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
+	
+	@Autowired
+	ConvidadoRepository convidadoRepository;
 	
 	@RequestMapping("/")
 	public String index(){
@@ -12,7 +22,12 @@ public class ConvidadoController {
 	}
 	
 	@RequestMapping("listaconvidados")
-	public String listaConvidados(){
+	public String listaConvidados(Model model){
+		
+		Iterable<Convidado> convidados = convidadoRepository.findAll();
+		
+		model.addAttribute("convidados", convidados);
+		
 		return "listaconvidados";
 	}
 
